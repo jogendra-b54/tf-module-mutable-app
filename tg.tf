8a4de58440e2c3dfbeb8006f2beeb22a68c3f54d
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-03 19:53:59 +0530

    Auto push

--- tg.tf
+++ tg.tf
@@ -16,5 +16,20 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
   port                = 8080
 }
 
-# Attach the Target group to the ALB ( frontend TG should be attached to public ALB and backend TG should attah to private ALB)
+# Cretes rule in the Listener
+resource "aws_lb_listener_rule" "app_rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = 100 
 
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.data.terraform_remote_state.alb.outputs.PRIVATE_HOSTED_ZONE_NAME"]
+    }
+  }
+}
+# Attach the Target group to the ALB ( frontend TG should be attached to public ALB and backend TG should attah to private ALB)
