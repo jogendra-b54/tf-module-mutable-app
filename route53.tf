5b63070be0a373b7f5afb5afff63948e7ef582ba
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-06 13:58:11 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "dns_record" {
  //zone_id = if LB_TYPE == internal ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : PUBLIC HOSTED ZONE
   
-  zone_id = var.LB_TYPE == internal ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
+  zone_id = var.LB_TYPE == "internal" ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
-  records = var.LB_TYPE == internal ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+  records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
 
   
