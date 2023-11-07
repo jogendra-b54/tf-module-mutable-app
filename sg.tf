bbe1e1e970659cc0367c9cf34c18e2a45b3fc7a1
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-07 16:08:52 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -19,13 +19,13 @@ resource "aws_security_group" "allows_app" {
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR ]     
   }
 
-  ingress {
-    description = "Health Check Port"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR ]     
-  } 
+  # ingress {
+  #   description = "Health Check Port"
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR ]     
+  # } 
 
   egress {
     from_port   = 0
