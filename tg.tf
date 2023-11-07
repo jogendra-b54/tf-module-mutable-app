9af3f56e0a46780fb0fe292ca1cc51cb35d96249
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-07 15:36:20 +0530

    Auto push

--- tg.tf
+++ tg.tf
@@ -1,7 +1,7 @@
 # Creates Target Group for the backend Components 
 resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}-tg"
-  port     = var.APP_PORT
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
   health_check {
@@ -22,7 +22,7 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
   count               =  local.INSTANCE_COUNT
   target_group_arn    = aws_lb_target_group.app.arn
   target_id           = element(local.INSTANCE_IDS,count.index) 
-  port                = 8080
+  port                = var.APP_PORT
 }
 
 # Generates Random Number
