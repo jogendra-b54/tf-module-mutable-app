16d9f955d7f49755300e6f9690950e4c2ace8aa3
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-07 15:52:25 +0530

    Auto push

--- tg.tf
+++ tg.tf
@@ -1,7 +1,7 @@
 # Creates Target Group for the backend Components 
 resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}-tg"
-  port     = 8080
+  port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
   health_check {
