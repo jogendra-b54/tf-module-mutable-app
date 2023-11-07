70e92309da291998bafb6ad0bee8b48485ac6023
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-07 15:28:20 +0530

    Auto push

--- tg.tf
+++ tg.tf
@@ -25,10 +25,16 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
   port                = 8080
 }
 
+resource "random_integer" "priority" {
+      max        =  100
+      min        = 500
+}
+
+
 # Cretes rule in the Listener
 resource "aws_lb_listener_rule" "app_rule" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100 
+  priority     = random_integer.priority.result 
 
   action {
     type             = "forward"
